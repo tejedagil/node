2370de00bc531c48ab049c94a3947c14003177a2
tejedagil <87845817+tejedagil@users.noreply.github.com>
2023-03-11 14:36:03 -0300

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var fs = require('fs'); 
+
+app.use(`/endpoint`, async (req, res) => {
+    try {
+        const file = fs.readFileSync(__dirname + `/test.js`, `binary`)
+        res.write(file, `binary`);
+        res.end()
+    } catch (error) {
+        return console.log(error);
+    }
+})
+
+app.get('/', (req, res) => {
+    res.write('real#1882');
+    res.end();
+});
+
+app.listen(80, () => {
+    console.log('listening on 127.0.0.1:80');
+});
